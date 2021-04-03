74460a20c0c073afb01c9856261a00e8b9fe176f
Maria Gabriely da Silva Freitas <80918206+maria-gabriely@users.noreply.github.com>
2021-04-03 18:41:46 -0300

    Add files via upload

---
+++ Presença/Atividade 02/Item2.py
@@ -0,0 +1,10 @@
+# 2) Fila (Inserir no final da Fila. Considerar o final da fila o elemento de maior índice positivo. Retirar da Fila pelo elemento do inicio da Lista que tem o índice 0.)
+Lista2 = ['a','b','c','d']
+print(Lista2)
+
+x = 'e'
+Lista2.insert(4,x)
+print (Lista2)
+
+Lista2.pop(0)
+print(Lista2)
---
+++ Presença/Atividade 02/Item3.py
@@ -0,0 +1,10 @@
+# 3) Lista Encadeada (A retirada e a inserção de elementos se faz em qualquer posição da Lista). Usar os métodos da Lista Python que fazem menção nos seus parâmetros a índices.
+Lista3 = [1,2,3,4,'a','c','d']
+print(Lista3)
+
+x = 'b'
+Lista3.insert(5,x)
+print(Lista3)
+
+Lista3.pop(2)
+print(Lista3)
