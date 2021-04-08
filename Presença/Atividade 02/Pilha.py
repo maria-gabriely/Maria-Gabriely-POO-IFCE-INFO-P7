fafdde64ee535b7e88f9fa8337bb450203b503d8
Maria Gabriely da Silva Freitas <80918206+maria-gabriely@users.noreply.github.com>
2021-04-08 15:26:09 -0300

    Update and rename Item1.py to Pilha.py

---
+++ Presença/Atividade 02/Pilha.py
@@ -0,0 +1,11 @@
+# Criar uma lista em Python para implementar as seguintes Estruturas de Dados:
+# 1) Pilha (inserir e retirar pelo topo da Pilha. Considerar o topo como sendo o índice 0 da Lista.
+
+Lista1 = [1,2,5,6,9]
+print(Lista1)
+x = 2
+Lista1.insert(0,x)
+print(Lista1)
+
+Lista1.pop(0)
+print(Lista1)
