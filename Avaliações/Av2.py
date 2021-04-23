e48aa5700c59285395f71cff8c79f9873ef3a67a
Maria Gabriely da Silva Freitas <80918206+maria-gabriely@users.noreply.github.com>
2021-04-23 18:25:33 -0300

    Update Av2.py

--- Avaliações/Av2.py
+++ Avaliações/Av2.py
@@ -1,3 +1,12 @@
+# O código Morse é um esquema de codificação que usa travessões e pontos para 
+# representar dígitos e letras. Nesta atividade, você escreverá um programa que 
+# usa um dicionário para armazenar o mapeamento desses símbolos para o código 
+# Morse. Use um ponto para representar um ponto e um hífen para representar um 
+# travessão.Seu programa deve ler uma mensagem do usuário. Em seguida, deve traduzir 
+# todas as letras e dígitos da mensagem para o código Morse, deixando um espaço
+#  entre cada sequência de travessões e pontos. Seu programa deve ignorar quaisquer 
+# caracteres que não estejam listados na tabela.
+
 Codigo_Morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-',
 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-',
