f33b6e6b25135f653622ea8e9f14e49b86b83812
Maria Gabriely da Silva Freitas <80918206+maria-gabriely@users.noreply.github.com>
2021-05-20 22:54:40 -0300

    Update TipoMovimento.py

--- Avaliações/Avaliação Prática/TipoMovimento.py
+++ Avaliações/Avaliação Prática/TipoMovimento.py
@@ -3,9 +3,3 @@ import enum
 class TipoMovimento(enum.Enum):
     Provento = 'P'
     Desconto = 'D'
-    
-if __name__ == '__main__':
-    print('As letras enum são:')
-    for tipo in (TipoMovimento):
-        print(type(tipo))
-        print(tipo)
