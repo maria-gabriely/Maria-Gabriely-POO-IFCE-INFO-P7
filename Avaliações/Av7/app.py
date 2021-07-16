61454425ff5ccbaf0c0b5cd2c69bae6be494278f
Maria Gabriely da Silva Freitas <80918206+maria-gabriely@users.noreply.github.com>
2021-07-15 23:45:37 -0300

    Update app.py

--- Avaliações/Av7/app.py
+++ Avaliações/Av7/app.py
@@ -1,9 +1,6 @@
-from enum import unique
-from flask import Flask, Response, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
-import json
-from sqlalchemy.orm import backref
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:**************@localhost/notafiscal'
